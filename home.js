e6fd160f8b4014e9972013918158ba39c6fbf901
Dawy678 <Dawios2006@wp.pl>
2021-05-23 15:12:08 +0200

    o

--- home.js
+++ home.js
@@ -1,22 +1,24 @@
 const app = document.getElementById('app')
-const imgduze1 = document.createElement('img')
-imgduze1.setAttribute('src', './img/GTA-6-PS4.jpg')
-imgduze1.setAttribute('id', 'imgduze1')
-app.appendChild(imgduze1)
-const informacjeogta6 = document.createElement('div')
-informacjeogta6.setAttribute('id', 'informacjeogta6')
-app.appendChild(informacjeogta6)
-const infogta6 = document.createElement('div')
-infogta6.setAttribute('id', 'infogta6')
-infogta6.innerText = "Informacje o GTA VI"
-informacjeogta6.appendChild(infogta6)
-const tekstgta6 = document.createElement('span')
-tekstgta6.setAttribute('id', 'tekstgta6')
-tekstgta6.innerText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur ad exercitationem ratione repellendus quo. Neque, et voluptatem. Quia voluptate sed quod repellat totam nobis deleniti ratione odio, commodi autem sequi saepe, dolorem voluptatum eligendi sint quas, accusamus impedit sit provident perferendis molestias inventore ex? Vero facere, reprehenderit sunt totam obcaecati iusto. Assumenda at autem nulla perferendis tempore! Perspiciatis, illum iusto! Veniam nostrum voluptatum, vero autem odit quaerat est, reiciendis vel rerum fugit neque inventore! Perferendis, iste ea. Commodi illo, excepturi distinctio facilis necessitatibus, nam quae nemo, maiores earum facere itaque in aspernatur enim. Excepturi, obcaecati nam! Officia accusantium perspiciatis beatae."
-informacjeogta6.appendChild(tekstgta6)
-// GALERIA
-const galeriabignapis = document.createElement('span')
-galeriabignapis.setAttribute('id', 'galeriabignapis')
-galeriabignapis.innerText = "Galeria"
-app.appendChild(galeriabignapis)
-// ZDJECIA W GALERII WOOOOOOOOHOOOOOOOOOO OOOOOOOOOOOOOOO
+const a = document.getElementById('search')
+a.style.display = "none"
+function searchOnclick() {
+    a.style.display = ""
+}
+function searchOnchange() {
+    a.style.display = "none"
+}
+function search() {
+    const input = document.getElementById('searchInput')
+    const filter = input.value.toUpperCase()
+    const ul = document.getElementById("search")
+    const li = ul.getElementsByTagName('li')
+    for (i = 0; i < li.length; i++) {
+        const a = li[i].getElementsByTagName("a")[0]
+        const txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = ""
+      } else {
+        li[i].style.display = "none"
+      }
+    }
+  }
